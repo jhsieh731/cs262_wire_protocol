ab4fe6ffb8760b29d0c596a998db7f75381e6447
Kayla Zethelyn <kaylazethelyn@college.harvard.edu>
2025-02-12 15:29:42 -0500

    create logs folder if not exist

--- logger.py
+++ logger.py
@@ -1,10 +1,14 @@
 import logging
+import os
 
 def set_logger(name, log_file):
     """Set up the logger with the given name and log file."""
     # Set up logging
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)  # Capture all levels
+
+    # Create logs directory if it doesn't exist
+    os.makedirs('logs', exist_ok=True)
 
     # Create file handler
     log_file = "logs/" + log_file
